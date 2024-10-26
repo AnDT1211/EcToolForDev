23ee1cfb05dbc991462465f131aaaf16d161fc9e
AnDT1211 <anthaido.bk@gmail.com>
2024-10-26 13:40:48 +0700

    update ta01

--- src/main/java/com/andt/ectool/controller/ta/Ta01ControllerRest.java
+++ src/main/java/com/andt/ectool/controller/ta/Ta01ControllerRest.java
@@ -1,25 +1,29 @@
 package com.andt.ectool.controller.ta;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("ta01/api")
 public class Ta01ControllerRest {
 
 	@PostMapping("/convert")
-	public String convertA5ToMd(@RequestBody String a5Input) {
-		List<String> ls = a5Input.lines().map(x -> x.replaceAll("\\t", "|")).collect(Collectors.toList());
-		int c = (int)ls.get(0).chars().filter(x -> x == '|').count();
-		String str = Stream.generate(() -> "-").limit(c + 1).collect(Collectors.joining("|"));
-		ls.add(1, str);
-		return ls.stream().collect(Collectors.joining("\n"));
+	public Map<String, Object> convertA5ToMd(HttpServletRequest request) {
+		String a5Input = request.getParameter("a5Input");
+		List<String> ls = a5Input.lines().collect(Collectors.toList());
+		Map<String, Object> output = new HashMap<>();
+		output.put("header", ls.get(0).split("\\t"));
+		ls.remove(0);
+		output.put("body", ls.stream().map(x -> x.split("\\t")).toArray());
+		return output;
 	}
 	
 }
--- src/main/resources/static/js/ta/ta01.js
+++ src/main/resources/static/js/ta/ta01.js
@@ -1,40 +1,54 @@
-/**
- * 
- */
-
-
-
 
 $(document).ready(function() {
-	$('#a5_result_with_tab').focusout(async function() {
+	$('#table_a5').hide();
+	
+	$('#convertBtn').on('click', function() {
+		showHide();
+
 		const a5Input = getA5Input();
-        const mdResult = await getMdResultFromA5Format(a5Input); 
-		
-		console.log(mdResult);
-        setValueToMD(mdResult);
+		console.log(a5Input);
+		$.ajax({
+			type: 'POST',
+			url: 'ta01/api/convert',
+			data: { a5Input: a5Input },
+			success: function(response) {
+				updateTable(response);
+			},
+			error: function(error) {
+				console.error('Error:', error);
+			}
+		});
 	});
 });
 
-function getA5Input() {
-	return $('#a5_result_with_tab').val().toString();
+function showHide() {
+	if(!$('#form_input').is(':visible')) {
+	    $('#form_input').show();
+	} else {
+		$('#form_input').hide();
+	}
+
+	if(!$('#table_a5').is(':visible')) {
+	    $('#table_a5').show();
+	} else {
+		$('#table_a5').hide();
+	}
 }
 
-function setValueToMD(a5Input) {
-	$("#md_result").text(a5Input);
-}
+function updateTable(data) {
+	$('#header_row').empty();
+	data.header.forEach(line => {
+		$('#header_row').append(`<th>${line}</th>`);
+	});
 
-async function getMdResultFromA5Format(a5Input) {
-	$.post("ta01/api/convert", 
-	    {
-	        a5Input: a5Input
-	    },
-	    function(response) {
-	        console.log("Response:", response);
-			return response.filter('#one').text();;
-	    }
-	).fail(function(error) {
-	    console.error("Error:", error);
-		return "";
+
+	$('#table_a5 tbody').empty();
+	data.body.forEach(row => {
+		let tds = row.map(cell => `<td>${cell}</td>`).join('');
+		$('#table_a5 tbody').append(`<tr>${tds}</tr>`);
 	});
 }
 
+function getA5Input() {
+	return $('#a5_input_with_tab').val().toString();
+}
